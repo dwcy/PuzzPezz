73efa0cfe2b87d33c104b4967bf0f4bb00c8cb20
dwcy <dawid@bahnhof.se>
2018-12-16 22:01:06 +0100

    player controller basic part 1

--- PlayerController.cs
+++ PlayerController.cs
@@ -18,7 +18,35 @@ public class PlayerController : MonoBehaviour
 
         else if (Input.GetKeyDown(KeyCode.DownArrow))
             Rotate(-180);
+    }
 
+    void Rotate(float z)
+    {
+        Vector3 currentRotation = transform.eulerAngles;
+        Vector3 nextRotation = transform.eulerAngles + new Vector3(0, 0, z);
+        transform.eulerAngles = Vector3.Lerp(currentRotation, nextRotation,  1f);
+        ChangeColorBasedOnAngle();
+    }
 
+      void ChangeColorBasedOnAngle()
+    {
+        
+        if (transform.eulerAngles.z >= 270)
+            ChangeColor(Color.green);
+
+        else if (transform.eulerAngles.z >= 180)
+            ChangeColor(Color.blue);
+
+        else if (transform.eulerAngles.z >= 90)
+            ChangeColor(Color.yellow);
+
+        else if (transform.eulerAngles.z >= 0)
+            ChangeColor(Color.red);
+
+    }
+
+     void ChangeColor(Color color)
+    {
+        GetComponent<SpriteRenderer>().material.color = color;
     }
 }
